5baae2378078c3371a616e5da4ea0014015d8507
Grey-Uti <146878048+Grey-Uti@users.noreply.github.com>
2025-04-16 09:47:15 -0600

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -1,3 +1,28 @@
+/*
+===================================================================================
+Stored Procedure: Load Silver Layer (Bronze -> Silver)
+===================================================================================
+
+Script Purpose:
+    This stored procedure performs the ETL (Extract, Transform, Load) process to
+    populate the 'silver' schema tables from the 'bronze' schema.
+Actions performed:
+    - Truncate the silver tables
+    - Inserts transformed and cleansed data from Bronze into silver tables.
+
+Parameters:
+    None.
+  This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    EXEC bronze.load_silver;
+===================================================================================
+
+*/
+
+
+
+
 USE DataWarehouse
 GO
 
